f99c49c99c50396e5066ace1fc17b3bfa043d137
vanshaj bansal <bnslgvansh67@gmail.com>
2025-09-01 10:41:15 +0530

    all set

--- js/contact.js
+++ js/contact.js
@@ -14,3 +14,40 @@ document.addEventListener('DOMContentLoaded', function () {
     adjustFormPosition();
     window.addEventListener('resize', adjustFormPosition);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Load footer dynamically
+    const footerElement = document.getElementById("footerImplement");
+    const navbarElement = document.getElementById("navbarImplement")
+    if (footerElement) {
+        fetch("footer.html")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch footer.html");
+                }
+                return response.text();
+            })
+            .then(data => {
+                footerElement.innerHTML = data;
+            })
+            .catch(error => console.error("Error loading footer:", error));
+    } else {
+        console.error("Footer element not found!");
+    }
+    if (navbarElement) {
+        fetch("Navbar2.html")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch Navbar.html");
+                }
+                return response.text();
+            })
+            .then(data => {
+                navbarElement.innerHTML = data;
+            })
+            .catch(error => console.error("Error loading navbar:", error));
+    } else {
+        console.error("Navbar element not found!");
+    }
+
+});
